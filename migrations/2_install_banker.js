97a92b3fa7226b0dc56eef18056df31bb3d5438c
Matthew Lee <mattxlee@gmail.com>
2019-03-25 19:19:30 +0800

    Add deployment scripts.

--- migrations/2_install_banker.js
+++ migrations/2_install_banker.js
@@ -1,5 +1,16 @@
 var Banker = artifacts.require("./Banker.sol");
+var Rou = artifacts.require('./Rou.sol');
+var KeyCalc = artifacts.require('./KeyCalc.sol');
+var SafeMath = artifacts.require('./SafeMath.sol');
 
 module.exports = function(deployer) {
+  deployer.deploy(Rou);
+  deployer.deploy(KeyCalc);
+  deployer.deploy(SafeMath);
+
+  deployer.link(Rou, Banker);
+  deployer.link(KeyCalc, Banker);
+  deployer.link(SafeMath, Banker);
+
   deployer.deploy(Banker);
 };
