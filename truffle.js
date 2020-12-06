4a876ede8a3964e4f470cd6ffa4674437b29e57b
Matthew Lee <mattxlee@gmail.com>
2020-12-07 05:45:32 +0800

    Change `ganache` to `test`.

--- truffle.js
+++ truffle.js
@@ -21,7 +21,7 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    ganache: {
+    test: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
