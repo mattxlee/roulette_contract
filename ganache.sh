cb5373a5f949b38157e4cef83b498a63c2a65ca9
Matthew Lee <mattxlee@gmail.com>
2020-12-06 19:13:33 +0800

    Add script and the truffle settings back.

---
+++ ganache.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -f .mnemonic ]; then
+    echo "You need generate the mnemonic and save to file .mnemonic before you start Ganache client."
+    exit 1
+fi
+
+ganache-cli -b 1 -p 7545 -i 101 -m "`cat .mnemonic`"
