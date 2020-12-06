a3086dc2fbdee698475c329140cbd586669d27da
Matthew Lee <mattxlee@gmail.com>
2020-12-07 05:45:05 +0800

    Fix the script error.

--- ganache.sh
+++ ganache.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -f .mnemonic ]; then
+if [ ! -f .mnemonic ]; then
     echo "You need generate the mnemonic and save to file .mnemonic before you start Ganache client."
     exit 1
 fi
