b589a981e4136a8fec04c64bf11f31e388cf23a4
Matthew Lee <mattxlee@gmail.com>
2020-12-07 15:58:27 +0800

    Improve the deployment related stuff.

--- truffle.js
+++ truffle.js
@@ -1,21 +1,7 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() {
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
-
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 const fs = require("fs");
-const mnemonic = fs.readFileSync(".mnemonic");
+const mnemonicPhrase = fs.readFileSync(".mnemonic").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -26,28 +12,8 @@ module.exports = {
       port: 7545,
       network_id: "*"
     },
-    rinkeby: {
-        provider: () => {
-            return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/daad1c45f9b6487288f56ff2bac9577a");
-        },
-        network_id: "*"
-    },
-    ropsten: {
-        provider: () => {
-            return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/daad1c45f9b6487288f56ff2bac9577a");
-        },
-        network_id: "*"
-    },
-    kovan: {
-        provider: () => {
-            return new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/daad1c45f9b6487288f56ff2bac9577a");
-        },
-        network_id: "*"
-    },
-    goerli: {
-        provider: () => {
-            return new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/daad1c45f9b6487288f56ff2bac9577a");
-        },
+    testnet: {
+        provider: new HDWalletProvider({mnemonic: { phrase: mnemonicPhrase }, providerOrUrl: "https://ropsten.infura.io/v3/daad1c45f9b6487288f56ff2bac9577a"}),
         network_id: "*"
     }
   }
